689a66aa9cbd90e9d7e0dafed11d090ed3f49cf8
Dinesh01-dev <dineshgs011@gmail.com>
2024-12-28 14:49:27 +0530

    second

---
+++ QuizGame/src/com/dk/config/QuizQuestions.java
@@ -0,0 +1,17 @@
+package com.dk.config;
+
+public class QuizQuestions {
+	public String questions;
+	public String[] options;
+	public int correctAnswer;
+	
+	
+	
+	public QuizQuestions(String questions, String[] options,int correctAnswer) {
+		this.questions=questions;
+		this.options=options;
+		this.correctAnswer=correctAnswer;
+	}//
+	
+
+}

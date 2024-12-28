e02030c927c476aa1a26497feef1fc01b9c79b94
Dinesh01-dev <dineshgs011@gmail.com>
2024-12-28 15:14:34 +0530

    final

--- QuizGame/src/com/dk/main/QuizTest.java
+++ QuizGame/src/com/dk/main/QuizTest.java
@@ -1,7 +1,11 @@
 package com.dk.main;
 
-import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import com.dk.config.QuizQuestions;
 
@@ -24,22 +28,34 @@ public class QuizTest {
 				System.out.println((i+1)+ "." + qQ.options[i]);
 			}
 			
-			System.out.println("Select Your Answer::");
-			int userAnswer=s.nextInt();
-			
-			
-			if(userAnswer==qQ.correctAnswer) {
-				System.out.println("Good Correct Answer!");
-				score++;
-			}
-			else {
-				System.out.println("Oops! Wrong Answwer "+"correct Answer:: "+qQ.options[qQ.correctAnswer-1]);
-			}
-			
-		}
-		System.out.println("Quiz is Over And your final Score "+score+ "/"+qQuestions.length);
-		s.close();
+System.out.println("Select Your Answer (You have 5 seconds):");
+            
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            Future<Integer> future = executor.submit(() -> s.nextInt());
 
-	}
+            int userAnswer = -1; // Default 
 
+            try {
+                
+                userAnswer = future.get(5, TimeUnit.SECONDS);
+            } catch (TimeoutException e) {
+                System.out.println("Time's up! Moving to the next question.");
+            } catch (Exception e) {
+                System.out.println("Invalid input! Moving to the next question.");
+            } finally {
+                executor.shutdownNow(); 
+            }
+
+           
+            if (userAnswer == qQ.correctAnswer) {
+                System.out.println("Good! Correct Answer!");
+                score++;
+            } else if (userAnswer != -1) { // Only display the correct answer if user answered
+                System.out.println("Oops! Wrong Answer. Correct Answer: " + qQ.options[qQ.correctAnswer - 1]);
+            }
+        }
+
+        System.out.println("Quiz is Over! Your final score: " + score + " / " + qQuestions.length);
+        s.close();
+    }
 }
